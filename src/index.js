1310bc21847b7bb8aaf1bbc23db5a709be70c2c3
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-03-13 22:53:16 +0530

    data base connections

---
+++ src/index.js
@@ -0,0 +1,59 @@
+//require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+// export const DB_NAME = "videotube";
+// import{DB_NAME} from "./constants"; 
+import connectDB from "./db/index.js";
+
+
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import { Express } from "express";
+const app = Express()
+
+
+( async() => {
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on("error", (error) => {
+        console.log("ERROR", Error);
+        throw error
+       })
+
+     app.listen(process.env.PORT, () => {
+        console.log(`app is listening on port ${process.env.PORT}`);
+     })
+
+    } catch (error) {
+        console.error("ERROR:". error)
+        throw err
+    }
+
+})()
+*/
