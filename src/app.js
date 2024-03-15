0ecc0cd3c89e5cfc18a02d41b1a560a639cd03aa
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-03-15 14:35:34 +0530

    cookies

--- src/app.js
+++ src/app.js
@@ -13,4 +13,10 @@ app.use(core({
 
 
 app.use(Express.json({limit: "16kb"}))
+app.use(Express.urlencoded({extended: true,limit:"16kb"}))
+app.use(Express.static("public"))
+app.use(cookieParser())
+
+
+
 export {app}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+ const asyncHandeler = (requestHandeler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandeler(req, res,next))
+        .catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandeler}
+
+// const asyncHandeler = (func) => () => {}
+
+// const asyncHandeler = (fn) => async (req) => {
+
+//     try {
+//       await fn(req, res, next)
+//     }catch (error) {
+//         resizeBy.status(err.code || 5000).json({
+//             success: false,
+//             message: RegExp.message
+
+//         })
+//     }
+// }
